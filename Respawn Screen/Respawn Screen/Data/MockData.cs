9ebe386647b5595c5c123cd98be46153559237ff
Andrei <andisavauserul@gmail.com>
2022-04-13 15:44:40 +0200

    Another attempt at putting the project on git

---
+++ Respawn Screen/Respawn Screen/Data/MockData.cs
@@ -0,0 +1,7 @@
+namespace Respawn_Screen.Data
+{
+    public class MockData
+    {
+        public Layout Payload = new Layout("Payload", new Marker[] { new Marker("Red objective", 30.54f, 193.23f) });
+    }
+}
---
+++ Respawn Screen/Respawn Screen/Data/Scenario.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Respawn_Screen.Data
+{
+    public class Scenario
+    {
+        public string Name;
+        public string Briefing = "";
+        public Layout[] Layouts { get => layouts.ToArray(); }
+        private List<Layout> layouts;
+
+        public Scenario(string name)
+        {
+            Name = name;
+            layouts = new List<Layout>();
+        }
+
+        public Scenario(string name, Layout[] layouts)
+        {
+            Name = name;
+            this.layouts = layouts.ToList();
+        }
+    }
+}
