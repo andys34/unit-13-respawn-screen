8ffeffe8ccedd775500a3a0a1968c4b75d751791
Skyline34 <35419029+andys34@users.noreply.github.com>
2022-04-13 18:21:58 +0300

    Delete Respawn Screen directory

--- Respawn Screen/Respawn Screen/Data/MockData.cs
+++
@@ -1,7 +0,0 @@
-namespace Respawn_Screen.Data
-{
-    public class MockData
-    {
-        public Layout Payload = new Layout("Payload", new Marker[] { new Marker("Red objective", 30.54f, 193.23f) });
-    }
-}
