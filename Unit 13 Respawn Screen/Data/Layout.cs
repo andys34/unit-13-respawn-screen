6b2706ff7f47657b120948ecd4960efc2dc26167
Andrei Sava <a.sava@student.fontys.nl>
2022-04-13 17:46:56 +0200

    Moved all files from "master" branch to "main" branch.

---
+++ Unit 13 Respawn Screen/Data/Layout.cs
@@ -0,0 +1,35 @@
+﻿namespace Unit_13_Respawn_Screen.Data
+{
+    public class Layout
+    {
+        public string Name;
+
+        public Marker[] Markers { get => markers.ToArray(); }
+        private List<Marker> markers = new List<Marker>();
+        public Layout(string name)
+        {
+            Name = name;
+        }
+
+        public Layout(string name, Marker[] markers)
+        {
+            Name = name;
+            this.markers = markers.ToList();
+        }
+        public Layout(string name, List<Marker> markers)
+        {
+            Name = name;
+            this.markers = markers;
+        }
+
+        public void AddMarker(Marker marker)
+        {
+            markers.Add(marker);
+        }
+
+        public void RemoveMarker(Marker marker)
+        {
+            markers.Remove(marker);
+        }
+    }
+}
